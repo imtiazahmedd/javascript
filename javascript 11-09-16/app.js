b7ea641d1d35fb86ed2e06b019e01e8b89073651
imtiazahmedd <imtiazpak65@gmail.com>
2016-09-17 10:13:33 -0700

    javascript practices

---
+++ javascript 11-09-16/app.js
@@ -0,0 +1,6 @@
+var first = prompt ("Enter 1st value","Enter 1st number");
+var first = parseInt(first);
+var second = prompt("Enter 2nd value","Enter 2nd number");
+var second = parseInt(second);
+var addition = first + second;
+document.write(addition);
---
+++ javascript 17-09-16/app.js
@@ -0,0 +1,12 @@
+var name = prompt("Enter your name","Name");
+
+
+var email = prompt("Enter your email","Email");
+
+
+var age = prompt("Enter your age","Age");
+
+
+var roll = prompt("Enter your roll no","Roll no");
+
+document.write("Name: "+ name +"<br>" + "Email: "+email+"<br>" +"Age: "+ age+"<br>" +"Roll:" ,"roll" );
