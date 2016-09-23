e67e40041b40d9853b07b8c0e1a7f9616e1af80d
imtiazahmedd <imtiazpak65@gmail.com>
2016-09-23 10:47:52 -0700

    javascript calculator and lab task done!

---
+++ js-calculator/app.js
@@ -0,0 +1,26 @@
+function myfunction1(){
+    var a =  parseInt(prompt("Enter 1st no for addtion","First Num"));
+    var b = parseInt(prompt("Enter 2nd no for addition","Second Num"));
+    alert(a+b);
+}
+
+function myfunction2(){
+    var a =  parseInt(prompt("Enter 1st no for subtraction","First Num"));
+    var b = parseInt(prompt("Enter 2nd no for subtraction","Second Num"));
+    alert(a-b);
+}
+function myfunction3(){
+    var a =  parseInt(prompt("Enter 1st no for multiplication","First Num"));
+    var b = parseInt(prompt("Enter 2nd no for multiplication","Second Num"));
+    alert(a*b);
+}
+function myfunction4(){
+    var a =  parseInt(prompt("Enter 1st no for division","First Num"));
+    var b = parseInt(prompt("Enter 2nd no for division","Second Num"));
+    alert(a/b);
+}
+function myfunction5(){
+    var a =  parseInt(prompt("Enter 1st no for modulus","First Num"));
+    var b = parseInt(prompt("Enter 2nd no for modulus","Second Num"));
+    alert(a%b);
+}
---
+++ lab task 24-09-16/app.js
@@ -0,0 +1,14 @@
+var name = prompt("Enter your name","name");
+document.getElementById("name").innerHTML = name;
+
+var email = prompt("Enter your email","email");
+document.getElementById("email").innerHTML = email;
+
+var address = prompt("Enter your address","address");
+document.getElementById("address").innerHTML = address;
+
+var mobile = prompt("Enter your mobile","mobile");
+document.getElementById("mobile").innerHTML = mobile;
+
+var education = prompt("Enter your education","education");
+document.getElementById("education").innerHTML = education;
